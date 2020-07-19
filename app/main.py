b50fa0492f433506b48612fa28197656ed8c4b13
Tieg <tiegosullivanpsnl@gmail.com>
2020-07-19 22:43:56 +0200

    .

--- app/main.py
+++ app/main.py
@@ -13,25 +13,24 @@ app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = 465,
     MAIL_USE_SSL = True,
-    MAIL_USERNAME = 'dummy999email@gmail.com',
-    MAIL_PASSWORD = 'idk'
+    MAIL_USERNAME = os.environ.get('email'),
+    MAIL_PASSWORD = os.environ.get('password')
 )
 mail = Mail(app)
 
 @app.route('/email/send', methods = ['GET', 'POST'])
 def send():
-    return os.environ.get('password')
-    # result = request.form
-    # subject = str(result['subject'])
-    # email = str(result['email'])
-    # body = str(result['body'])
+    result = request.form
+    subject = str(result['subject'])
+    email = str(result['email'])
+    body = str(result['body'])
 
-    # msg = Message('Mail from your Website', sender='jamiejobobrien@gmail.com', recipients=['tiegosullivanpsnl@gmail.com'] )
-    # msg.body = 'From: ' + email + 'Subject: ' + subject + "\n\n" + body
-    # msg.subject = subject
-    # mail.send(msg)
+    msg = Message('Mail from your Website', sender='jamiejobobrien@gmail.com', recipients=['tiegosullivanpsnl@gmail.com'] )
+    msg.body = 'From: ' + email + 'Subject: ' + subject + "\n\n" + body
+    msg.subject = subject
+    mail.send(msg)
 
-    # return '<h1>Message sent</h1>'
+    return '<h1>Message sent</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
