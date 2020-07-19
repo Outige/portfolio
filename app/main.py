036865f4fd1ed6f77697deb01cbf535d84fe6dd5
Tieg <tiegosullivanpsnl@gmail.com>
2020-07-19 23:41:51 +0200

    :sparkles: message screne

--- app/main.py
+++ app/main.py
@@ -9,28 +9,35 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/message/response')
+def message():
+    return render_template('message.html')
+
 app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = 465,
     MAIL_USE_SSL = True,
-    MAIL_USERNAME = os.environ.get('email'),
+    MAIL_USERNAME = 'dummy999@gmail.com',
     MAIL_PASSWORD = os.environ.get('password')
 )
 mail = Mail(app)
 
 @app.route('/email/send', methods = ['GET', 'POST'])
 def send():
-    result = request.form
-    subject = str(result['subject'])
-    email = str(result['email'])
-    body = str(result['body'])
+    try:
+        result = request.form
+        subject = str(result['subject'])
+        email = str(result['email'])
+        body = str(result['body'])
 
-    msg = Message('Mail from your Website', sender='jamiejobobrien@gmail.com', recipients=['tiegosullivanpsnl@gmail.com'] )
-    msg.body = 'From: ' + email + 'Subject: ' + subject + "\n\n" + body
-    msg.subject = subject
-    mail.send(msg)
+        msg = Message('Mail from your Website', sender='dummy999@gmail.com', recipients=['tiegosullivanpsnl@gmail.com'] )
+        msg.body = 'From: ' + email + 'Subject: ' + subject + "\n\n" + body
+        msg.subject = subject
+        mail.send(msg)
+    except:
+        return render_template('message.html', msg="Something went wrong")
 
-    return '<h1>Message sent</h1>'
+    return render_template('message.html', msg="Message received, Thank you!")
 
 if __name__ == "__main__":
     app.run(debug=True)
